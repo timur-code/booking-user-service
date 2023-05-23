e4bd100cc2943d90dd59a8b86ee89aaeadc7e961
Timur Umurzakov <goust327@gmail.com>
2023-05-24 03:44:54 +0600

    Changed admins

--- src/main/java/aitu/booking/userService/service/UserService.java
+++ src/main/java/aitu/booking/userService/service/UserService.java
@@ -96,9 +96,10 @@ public class UserService {
     //TODO:TEST
     public CreateRestaurantAdminDTO createRestaurantAdmin(CreateRestaurantAdminDTO adminDTO, String password, String token) throws InstanceAlreadyExistsException {
         if (!token.equals(serviceToken)) {
-            log.error("\ntoken: {}, \nreceived:{}", serviceToken, token);
+            log.error("\ntoken: {}, \nreceived:{}", token, serviceToken);
             throw new ApiException(403, "token.required");
         }
+        log.info("Res admin: {}", adminDTO);
         UserRepresentation userRepresentation = KeycloakUtils.convertToUserRepresentation(adminDTO);
         userRepresentation.setEnabled(true);
         KeycloakUtils.setUserRepresentationPassword(userRepresentation, password);
